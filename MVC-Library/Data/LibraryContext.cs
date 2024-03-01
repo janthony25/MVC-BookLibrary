d3e559f55d09a9a2637126a9db9f24952658d3ba
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-02 09:08:51 +1300

    Scaffold datas

---
+++ MVC-Library/Data/LibraryContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC_Library.Models;
+
+namespace MVC_Library.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext (DbContextOptions<LibraryContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC_Library.Models.Book> Book { get; set; } = default!;
+    }
+}
--- MVC-Library/Program.cs
+++ MVC-Library/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVC_Library.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<LibraryContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("LibraryContext") ?? throw new InvalidOperationException("Connection string 'LibraryContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
